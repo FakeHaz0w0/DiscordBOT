763a4b67e17c11f4adcf335cfd629e17b12800b3
FakeHaz0w0 <hazminecraft03@gmail.com>
2025-09-16 20:23:05 +0800

    keep_alive.py

--- Hazsbot/keep_alive.py
+++ Hazsbot/keep_alive.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Hazsbot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
